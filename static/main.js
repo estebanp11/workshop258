e9ab9265ff63b6a38add915363c6f7593787f7e3
Maria Fernanda Garcia Pineda <maria.fernanda.garcia1@hp.com>
2020-09-15 20:28:05 -0500

    login Html Js

--- static/main.js
+++ static/main.js
@@ -2,6 +2,34 @@
 //   .then(res => res.json())
 //   .then(res => console.warn(res));
 
+function login(){
+  const correo = document.getElementById("correo").value;
+  const clave = document.getElementById("contrasena").value;
+
+  if (correo && clave)
+  {
+       var url = '/api/login';
+       var data = {correo: correo, clave: clave};
+
+        fetch(url, {
+        method: 'POST', 
+        body: JSON.stringify(data), 
+        headers:{
+         'Content-Type': 'application/json'
+        }
+         }).then(res => res.json())
+        .catch(error => console.error('Error:', error))
+        .then(response => console.log('Success:', response));
+ }
+  else
+  {
+    alert("Debe ingresar todos los datos");
+  }
+
+function openLogin(){
+  windowlocation.href = 'login.html';
+}
+
 function saveUser(){
   const nombre = document.getElementById("nombre").value;
   const apellido = document.getElementById("apellido").value;
@@ -9,7 +37,7 @@ function saveUser(){
   const clave = document.getElementById("contrasena").value;
   const rol = 2;
 
-  if (nombre && apellido && correo && contrasena && rol)
+  if (nombre && apellido && correo && clave && rol)
   {
        var url = '/api/usuarios';
        var data = {nombre: nombre, apellido: apellido, correo: correo, clave: clave, rol: rol};
