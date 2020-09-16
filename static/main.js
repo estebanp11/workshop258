8f37407f133c37166a4e672d7ed8482d3dd9e7fe
Maria Fernanda Garcia Pineda <maria.fernanda.garcia1@hp.com>
2020-09-15 20:14:21 -0500

    save Users from Html Js

--- static/main.js
+++ static/main.js
@@ -1,3 +1,32 @@
-fetch('/api/autores?user=admin')
-  .then(res => res.json())
-  .then(res => console.warn(res));
+// fetch('/api/autores?user=admin')
+//   .then(res => res.json())
+//   .then(res => console.warn(res));
+
+function saveUser(){
+  const nombre = document.getElementById("nombre").value;
+  const apellido = document.getElementById("apellido").value;
+  const correo = document.getElementById("correo").value;
+  const clave = document.getElementById("contrasena").value;
+  const rol = 2;
+
+  if (nombre && apellido && correo && contrasena && rol)
+  {
+       var url = '/api/usuarios';
+       var data = {nombre: nombre, apellido: apellido, correo: correo, clave: clave, rol: rol};
+
+        fetch(url, {
+        method: 'POST', 
+        body: JSON.stringify(data), 
+        headers:{
+         'Content-Type': 'application/json'
+        }
+         }).then(res => res.json())
+        .catch(error => console.error('Error:', error))
+        .then(response => console.log('Success:', response));
+ }
+  else
+  {
+    alert("Debe ingresar todos los datos");
+  }
+
+}
